5e07ccdd45d90f95a37ec7b2ed3f0b3ecad1fd30
anton <antontishchenko@gmail.com>
2019-03-02 09:53:36 +0200

    Demo project structure setup

---
+++ gulp-config.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+    var config = {
+        webRoot: "C:\\inetpub\\wwwroot\\sc910.sc\\",
+        devRoot: "C:\\2019-Incognito\\",
+        solutionName: "Emotion",
+        buildConfiguration: "Debug",
+        MSBuildToolsVersion: 'auto'
+    }
+    return config;
+}
---
+++ src/Foundation/LexisAnalysis/code/Facets/EmotionFacet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Sitecore.XConnect;
+
+namespace Emotion.Foundation.LexisAnalysis.Facets
+{
+    [Serializable]
+    [FacetKey(DefaultFacetKey)]
+    public class EmotionFacet : Facet
+    {
+        public const string DefaultFacetKey = "EmotionFacet";
+
+        // ReSharper disable once EmptyConstructor
+        // All facets must declare an empty constructor - this is an oData restriction. The constructor can be private.
+        public EmotionFacet()
+        {
+
+        }
+
+        public string Feeling { get; set; }
+
+        public float Anger { get; set; }
+        public float Disgust { get; set; }
+        public float Fear { get; set; }
+        public float Happiness { get; set; }
+        public float Neutral { get; set; }
+        public float Sadness { get; set; }
+        public float Surprise { get; set; }
+    }
+}
