451f408818889460466e2baa7115d1b6c2eed563
anton <antontishchenko@gmail.com>
2019-03-02 11:57:44 +0200

    Demo project form, initial setup

---
+++ src/Project/Demo/code/Controllers/FormController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Emotion.Project.Demo.Model;
+
+namespace Emotion.Project.Demo.Controllers
+{
+    public class FormController : Controller
+    {
+        public ActionResult Feedback()
+        {
+            var model = new Feedback();
+            return View(model);
+        }
+    }
+}
---
+++ src/Project/Demo/code/Model/Feedback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Emotion.Project.Demo.Model
+{
+    public class Feedback
+    {
+        [DisplayName("Name")]
+        public string Name { get; set; }
+        [DisplayName("Feedback")]
+        public string Text { get; set; }
+    }
+}
