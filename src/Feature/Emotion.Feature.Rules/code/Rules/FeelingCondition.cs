256d2a4bf97f608d43082091a71622ae8fc602bd
anton <antontishchenko@gmail.com>
2019-03-02 17:28:38 +0200

    Feature Rules project structure

---
+++ src/Feature/Emotion.Feature.Rules/code/Rules/FeelingCondition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sitecore.Rules;
+using Sitecore.Rules.Conditions;
+
+namespace Emotion.Feature.Rules.Rules
+{
+    public class FeelingCondition <T> : StringOperatorCondition<T> where T : RuleContext
+    {
+        public string Value
+        {
+            get;
+            set;
+        }
+
+        protected override bool Execute(T ruleContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
