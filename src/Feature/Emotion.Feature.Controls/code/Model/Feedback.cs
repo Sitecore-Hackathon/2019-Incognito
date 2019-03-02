5b85d4b5755bfa0f7280f974521ad4a80012ec47
anton <antontishchenko@gmail.com>
2019-03-02 22:18:37 +0200

    Prettify HTML

--- src/Feature/Emotion.Feature.Controls/code/Model/Feedback.cs
+++ src/Feature/Emotion.Feature.Controls/code/Model/Feedback.cs
@@ -1,12 +1,8 @@
-﻿using System.ComponentModel;
-
-namespace Emotion.Feature.Rules.Model
+﻿namespace Emotion.Feature.Rules.Model
 {
     public class Feedback
     {
-        [DisplayName("Name")]
         public string Name { get; set; }
-        [DisplayName("Feedback")]
         public string Text { get; set; }
     }
 }
